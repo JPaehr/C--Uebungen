406a3eaff19303c52c7434bd57b2a0cb04fc0947
JPaehr <j.paehr@gmx.de>
2014-09-25 20:36:27 +0200

    added smarter solution, added problem definition

--- Bierdeckel/Bierdeckel.cpp
+++ Bierdeckel/Bierdeckel.cpp
@@ -11,26 +11,39 @@ using namespace std;
 
 int deckelEbene(int ebene);
 int summeEbenen( int ebene);
+long long getNumberOfCards(long long levels);
+/*Exercise
+ * How many cards do i need to build chart house to defined level?
+*/
 
-int main(){
-	cout << summeEbenen(3);
-	return 0;
+//short solution ->faster, can takes more digits
+long long getNumberOfCards(long long levels){
+    //cards of one level = 2*level + level-1
+    //with Gauss...
+    return -levels+3*(levels*levels+levels)/2;
 }
+//long solution
 int deckelEbene(int ebene){
-	int startwert = 2;
+    int startwert = 2;
 
-	for(int i = 1; i < ebene;i++){
-		startwert += 3;
-	}
-	return startwert;
+    for(int i = 1; i < ebene;i++){
+        startwert += 3;
+    }
+    return startwert;
 }
 int summeEbenen(int ebene){
-	int startwert = 0;
+    int startwert = 0;
 
-	for(int i = 1; i <= ebene; i++){
-		startwert+= deckelEbene(i);
-	}
-	return startwert;
+    for(int i = 1; i <= ebene; i++){
+        startwert+= deckelEbene(i);
+    }
+    return startwert;
+}
+
+int main(){
+    cout << summeEbenen(3) << endl;
+    cout << getNumberOfCards(3) << endl;
+	return 0;
 }
 
 
